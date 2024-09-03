d3b89a0d3764a06b5a5db43d47ea2fa96c623b31
Wei Lim <weikanglim@gmail.com>
2024-09-03 09:25:48 -0700

    adjust theme and basic settings

--- init.lua
+++ init.lua
@@ -1,107 +1,107 @@
---[[
-
-=====================================================================
-==================== READ THIS BEFORE CONTINUING ====================
-=====================================================================
-========                                    .-----.          ========
-========         .----------------------.   | === |          ========
-========         |.-""""""""""""""""""-.|   |-----|          ========
-========         ||                    ||   | === |          ========
-========         ||   KICKSTART.NVIM   ||   |-----|          ========
-========         ||                    ||   | === |          ========
-========         ||                    ||   |-----|          ========
-========         ||:Tutor              ||   |:::::|          ========
-========         |'-..................-'|   |____o|          ========
-========         `"")----------------(""`   ___________      ========
-========        /::::::::::|  |::::::::::\  \ no mouse \     ========
-========       /:::========|  |==hjkl==:::\  \ required \    ========
-========      '""""""""""""'  '""""""""""""'  '""""""""""'   ========
-========                                                     ========
-=====================================================================
-=====================================================================
-
-What is Kickstart?
-
-  Kickstart.nvim is *not* a distribution.
-
-  Kickstart.nvim is a starting point for your own configuration.
-    The goal is that you can read every line of code, top-to-bottom, understand
-    what your configuration is doing, and modify it to suit your needs.
-
-    Once you've done that, you can start exploring, configuring and tinkering to
-    make Neovim your own! That might mean leaving Kickstart just the way it is for a while
-    or immediately breaking it into modular pieces. It's up to you!
-
-    If you don't know anything about Lua, I recommend taking some time to read through
-    a guide. One possible example which will only take 10-15 minutes:
-      - https://learnxinyminutes.com/docs/lua/
-
-    After understanding a bit more about Lua, you can use `:help lua-guide` as a
-    reference for how Neovim integrates Lua.
-    - :help lua-guide
-    - (or HTML version): https://neovim.io/doc/user/lua-guide.html
-
-Kickstart Guide:
-
-  TODO: The very first thing you should do is to run the command `:Tutor` in Neovim.
-
-    If you don't know what this means, type the following:
-      - <escape key>
-      - :
-      - Tutor
-      - <enter key>
-
-    (If you already know the Neovim basics, you can skip this step.)
-
-  Once you've completed that, you can continue working through **AND READING** the rest
-  of the kickstart init.lua.
-
-  Next, run AND READ `:help`.
-    This will open up a help window with some basic information
-    about reading, navigating and searching the builtin help documentation.
-
-    This should be the first place you go to look when you're stuck or confused
-    with something. It's one of my favorite Neovim features.
-
-    MOST IMPORTANTLY, we provide a keymap "<space>sh" to [s]earch the [h]elp documentation,
-    which is very useful when you're not exactly sure of what you're looking for.
-
-  I have left several `:help X` comments throughout the init.lua
-    These are hints about where to find more information about the relevant settings,
-    plugins or Neovim features used in Kickstart.
-
-   NOTE: Look for lines like this
-
-    Throughout the file. These are for you, the reader, to help you understand what is happening.
-    Feel free to delete them once you know what you're doing, but they should serve as a guide
-    for when you are first encountering a few different constructs in your Neovim config.
-
-If you experience any errors while trying to install kickstart, run `:checkhealth` for more info.
-
-I hope you enjoy your Neovim journey,
-- TJ
-
-P.S. You can delete this when you're done too. It's your config now! :)
---]]
-
--- Set <space> as the leader key
--- See `:help mapleader`
---  NOTE: Must happen before plugins are loaded (otherwise wrong leader will be used)
-vim.g.mapleader = ' '
-vim.g.maplocalleader = ' '
-
--- Set to true if you have a Nerd Font installed and selected in the terminal
-vim.g.have_nerd_font = false
-
--- [[ Setting options ]]
--- See `:help vim.opt`
--- NOTE: You can change these options as you wish!
---  For more options, you can see `:help option-list`
-
--- Make line numbers default
+-- --[[
+--
+-- =====================================================================
+-- ==================== READ THIS BEFORE CONTINUING ====================
+-- =====================================================================
+-- ========                                    .-----.          ========
+-- ========         .----------------------.   | === |          ========
+-- ========         |.-""""""""""""""""""-.|   |-----|          ========
+-- ========         ||                    ||   | === |          ========
+-- ========         ||   KICKSTART.NVIM   ||   |-----|          ========
+-- ========         ||                    ||   | === |          ========
+-- ========         ||                    ||   |-----|          ========
+-- ========         ||:Tutor              ||   |:::::|          ========
+-- ========         |'-..................-'|   |____o|          ========
+-- ========         `"")----------------(""`   ___________      ========
+-- ========        /::::::::::|  |::::::::::\  \ no mouse \     ========
+-- ========       /:::========|  |==hjkl==:::\  \ required \    ========
+-- ========      '""""""""""""'  '""""""""""""'  '""""""""""'   ========
+-- ========                                                     ========
+-- =====================================================================
+-- =====================================================================
+--
+-- What is Kickstart?
+--
+--   Kickstart.nvim is *not* a distribution.
+--
+--   Kickstart.nvim is a starting point for your own configuration.
+--     The goal is that you can read every line of code, top-to-bottom, understand
+--     what your configuration is doing, and modify it to suit your needs.
+--
+--     Once you've done that, you can start exploring, configuring and tinkering to
+--     make Neovim your own! That might mean leaving Kickstart just the way it is for a while
+--     or immediately breaking it into modular pieces. It's up to you!
+--
+--     If you don't know anything about Lua, I recommend taking some time to read through
+--     a guide. One possible example which will only take 10-15 minutes:
+--       - https://learnxinyminutes.com/docs/lua/
+--
+--     After understanding a bit more about Lua, you can use `:help lua-guide` as a
+--     reference for how Neovim integrates Lua.
+--     - :help lua-guide
+--     - (or HTML version): https://neovim.io/doc/user/lua-guide.html
+--
+-- Kickstart Guide:
+--
+--   TODO: The very first thing you should do is to run the command `:Tutor` in Neovim.
+--
+--     If you don't know what this means, type the following:
+--       - <escape key>
+--       - :
+--       - Tutor
+--       - <enter key>
+--
+--     (If you already know the Neovim basics, you can skip this step.)
+--
+--   Once you've completed that, you can continue working through **AND READING** the rest
+--   of the kickstart init.lua.
+--
+--   Next, run AND READ `:help`.
+--     This will open up a help window with some basic information
+--     about reading, navigating and searching the builtin help documentation.
+--
+--     This should be the first place you go to look when you're stuck or confused
+--     with something. It's one of my favorite Neovim features.
+--
+--     MOST IMPORTANTLY, we provide a keymap "<space>sh" to [s]earch the [h]elp documentation,
+--     which is very useful when you're not exactly sure of what you're looking for.
+--
+--   I have left several `:help X` comments throughout the init.lua
+--     These are hints about where to find more information about the relevant settings,
+--     plugins or Neovim features used in Kickstart.
+--
+--    NOTE: Look for lines like this
+--
+--     Throughout the file. These are for you, the reader, to help you understand what is happening.
+--     Feel free to delete them once you know what you're doing, but they should serve as a guide
+--     for when you are first encountering a few different constructs in your Neovim config.
+--
+-- If you experience any errors while trying to install kickstart, run `:checkhealth` for more info.
+--
+-- I hope you enjoy your Neovim journey,
+-- - TJ
+--
+-- P.S. You can delete this when you're done too. It's your config now! :)
+-- --]]
+--
+-- -- Set <space> as the leader key
+-- -- See `:help mapleader`
+-- --  NOTE: Must happen before plugins are loaded (otherwise wrong leader will be used)
+-- vim.g.mapleader = ' '
+-- vim.g.maplocalleader = ' '
+--
+-- -- Set to true if you have a Nerd Font installed and selected in the terminal
+-- vim.g.have_nerd_font = false
+--
+-- -- [[ Setting options ]]
+-- -- See `:help vim.opt`
+-- -- NOTE: You can change these options as you wish!
+-- --  For more options, you can see `:help option-list`
+--
+-- -- Make line numbers default
 vim.opt.number = true
--- You can also add relative line numbers, to help with jumping.
---  Experiment for yourself to see if you like it!
+-- -- You can also add relative line numbers, to help with jumping.
+-- --  Experiment for yourself to see if you like it!
 -- vim.opt.relativenumber = true
 
 -- Enable mouse mode, can be useful for resizing splits for example!
@@ -190,8 +190,33 @@ vim.keymap.set('n', '<C-l>', '<C-w><C-l>', { desc = 'Move focus to the right win
 vim.keymap.set('n', '<C-j>', '<C-w><C-j>', { desc = 'Move focus to the lower window' })
 vim.keymap.set('n', '<C-k>', '<C-w><C-k>', { desc = 'Move focus to the upper window' })
 
+vim.keymap.set('i', 'kj', '<Esc>')
 -- [[ Basic Autocommands ]]
 --  See `:help lua-guide-autocommands`
+
+-- Golang autoformat text
+vim.api.nvim_create_autocmd('BufWritePre', {
+  pattern = '*.go',
+  callback = function()
+    local params = vim.lsp.util.make_range_params()
+    params.context = { only = { 'source.organizeImports' } }
+    -- buf_request_sync defaults to a 1000ms timeout. Depending on your
+    -- machine and codebase, you may want longer. Add an additional
+    -- argument after params if you find that you have to write the file
+    -- twice for changes to be saved.
+    -- E.g., vim.lsp.buf_request_sync(0, "textDocument/codeAction", params, 3000)
+    local result = vim.lsp.buf_request_sync(0, 'textDocument/codeAction', params)
+    for cid, res in pairs(result or {}) do
+      for _, r in pairs(res.result or {}) do
+        if r.edit then
+          local enc = (vim.lsp.get_client_by_id(cid) or {}).offset_encoding or 'utf-16'
+          vim.lsp.util.apply_workspace_edit(r.edit, enc)
+        end
+      end
+    end
+    vim.lsp.buf.format { async = false }
+  end,
+})
 
 -- Highlight when yanking (copying) text
 --  Try it with `yap` in normal mode
@@ -255,7 +280,14 @@ require('lazy').setup({
       },
     },
   },
-
+  -- lazy.nvim
+  {
+    'm4xshen/hardtime.nvim',
+    dependencies = { 'MunifTanjim/nui.nvim', 'nvim-lua/plenary.nvim' },
+    opts = {
+      restriction_mode = 'hint',
+    },
+  },
   -- NOTE: Plugins can also be configured to run Lua code when they are loaded.
   --
   -- This is often very useful to both group configuration, as well as handle
@@ -606,7 +638,7 @@ require('lazy').setup({
       --        For example, to see the options for `lua_ls`, you could go to: https://luals.github.io/wiki/settings/
       local servers = {
         -- clangd = {},
-        -- gopls = {},
+        gopls = {},
         -- pyright = {},
         -- rust_analyzer = {},
         -- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
@@ -829,18 +861,21 @@ require('lazy').setup({
     -- change the command in the config to whatever the name of that colorscheme is.
     --
     -- If you want to see what colorschemes are already installed, you can use `:Telescope colorscheme`.
-    'folke/tokyonight.nvim',
+    -- 'folke/tokyonight.nvim',
+    'catppuccin/nvim',
+    name = 'catppuccin',
     priority = 1000, -- Make sure to load this before all the other start plugins.
     init = function()
       -- Load the colorscheme here.
       -- Like many other themes, this one has different styles, and you could load
       -- any other, such as 'tokyonight-storm', 'tokyonight-moon', or 'tokyonight-day'.
-      vim.cmd.colorscheme 'tokyonight-night'
+      vim.cmd.colorscheme 'catppuccin-mocha'
 
       -- You can configure highlights by doing something like:
       vim.cmd.hi 'Comment gui=none'
     end,
   },
+  { 'ThePrimeagen/vim-be-good' },
 
   -- Highlight todo, notes, etc in comments
   { 'folke/todo-comments.nvim', event = 'VimEnter', dependencies = { 'nvim-lua/plenary.nvim' }, opts = { signs = false } },
